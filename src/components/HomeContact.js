d4544507e16df9995a4d721da1c1867031d5f635
MateuszSzy <tasior19xx@gmail.com>
2021-11-23 16:24:31 +0100

    fourth commit

--- src/components/HomeContact.js
+++ src/components/HomeContact.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import "./../scss/styles/HomeContact.scss";
 import decoration from "../assets/Decoration.svg";
-import contact from "../assets/Background-Contact-Form.jpg";
-import facebook from "../assets/Facebook.svg";
-import instagram from "../assets/Instagram.svg";
+import sweater from "../assets/Background-Contact-Form.jpg";
 
 
 const HomeContact = () => {
     return (
             <div className="contact__container" id="contact">
+                <div className="contact__pic">
+                    <img className="contact__pic__show" src={sweater} alt="sweater" />
+                </div>
                 <div className="contents">
                     <p className="contents__title">Skontaktuj się z nami</p>
                     <img className="contents__decoration" src={decoration} alt="Logo" />
                     <div className="contents__form">
                         <div className="contents__form__date">
-                            <input placeholder="Imię" type="text"/>
+                            <label for="name">Do you like cheese?</label>
+                            <input placeholder="Imię" type="text" name="name"/>
                             <input placeholder="Nazwisko" type="text"/>
                         </div>
                         <input
@@ -23,9 +25,7 @@ const HomeContact = () => {
                         />
                         <button className="contents__form__btn">Wyślij</button>
                     </div>
-
                 </div>
-
             </div>
     );
 };
--- src/components/HomeFooter.js
+++ src/components/HomeFooter.js
@@ -5,11 +5,13 @@ import instagram from "../assets/Instagram.svg";
 
 const HomeFooter = () => {
     return (
-        <div className="contact__footer">
-            <p>Copyright by Coders Lab</p>
-            <img src={facebook} alt="facebook" />
-            <img src={instagram} alt="instagram" />
-        </div>
+            <div className="contact__footer">
+                <p className="contact__footer__title">Copyright by Coders Lab</p>
+                <div className="contact__footer__icon">
+                    <img src={facebook} alt="facebook" />
+                    <img src={instagram} alt="instagram" />
+                </div>
+            </div>
     );
 };
 
