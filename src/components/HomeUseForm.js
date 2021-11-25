2f54241edab7971f8624ee7be4a3202ed2454921
MateuszSzy <tasior19xx@gmail.com>
2021-11-25 18:24:33 +0100

    eight commit

--- src/components/HomeUseForm.js
+++ src/components/HomeUseForm.js
@@ -1,8 +1,17 @@
 import {useState, useEffect} from "react";
+import {db} from "../firebase";
+
 
 
 
 const HomeUseForm = validate => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [loader,setLoader] = useState(false);
+
+
     const [values, setValues] = useState({
         name: '',
         email: '',
@@ -25,6 +34,25 @@ const HomeUseForm = validate => {
         e.preventDefault();
 
         setErrors(validate(values));
+        setLoader(true)
+
+        db.collection('contacts').add({
+            name:values.name,
+            email:values.email,
+            message:values.message,
+        })
+            .then(() => {
+                alert('The request has been sent');
+                setLoader(false);
+            })
+            .catch((error) => {
+                alert(error.message);
+                setLoader(false);
+            });
+
+        setName("");
+        setEmail("");
+        setMessage("");
     };
 
     return {handleChange, values, handleSubmit, errors};
